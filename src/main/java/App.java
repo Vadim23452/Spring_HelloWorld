e18dc16712f0e3a27e5446ea954f554881215bfa
Vadim <vadim23452@yandex.ru>
2023-11-25 20:32:39 +0300

    The task is completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,26 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+            (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beenCat1 =
+            (Cat) applicationContext.getBean("cat");
+        System.out.println(beenCat1.getMassage());
+
+        Cat beenCat2 =
+            (Cat) applicationContext.getBean("cat");
+        System.out.println(beenCat2.getMassage());
+
+        System.out.println(bean == bean1);
+        System.out.println(beenCat1 == beenCat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+
+public class Cat {
+
+  private String hello;
+
+  public String getMassage() {
+    return hello;
+  }
+
+  public void setMassage(String hello) {
+    this.hello = hello;
+  }
+}
